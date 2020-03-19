1fc793aeac60635564074d5be36dd6670c399aa1
张博 <852972083@qq.com>
2020-03-19 09:24:01 +0800

    fix issue

--- component/dropdownmenu/dropdownmenu.js
+++ component/dropdownmenu/dropdownmenu.js
@@ -1,4 +1,3 @@
-
 Component({
   properties: {
     dropDownMenuTitle: {
@@ -6,10 +5,14 @@ Component({
       value: [],
     },
     dropDownMenuDistrictData: {
-      type:Array,
-      value:[]
+      type: Array,
+      value: [],
+      observer: function(newVal, oldVal) {
+        let model = newVal[0] ? newVal[0] : null
+        this.selectDefaltDistrictLeft(model)
+      }
     },
-   
+
     dropDownMenuSourceData: {
       type: Array,
       value: []
@@ -28,22 +31,22 @@ Component({
     district_open: false, // 区域
     source_open: false, // 来源
     style_open: false, // 出租 出售
-    filteropen: false,  // 筛选
+    filteropen: false, // 筛选
     shownavindex: '',
     dropDownMenuDistrictDataRight: {},
     district_left_select: '',
     district_right_select: '',
-    district_right_select_name:'',
+    district_right_select_name: '',
     selected_style_id: 0,
-    selected_style_name:'',
+    selected_style_name: '',
     selected_source_id: 0,
-    selected_source_name:'',
+    selected_source_name: '',
     selected_filter_id: 0,
     selected_filter_name: ''
   },
   methods: {
-    
-    tapDistrictNav: function (e) {
+
+    tapDistrictNav: function(e) {
       if (this.data.district_open) {
         this.setData({
           district_open: false,
@@ -63,7 +66,7 @@ Component({
       }
 
     },
-    tapSourceNav: function (e) {
+    tapSourceNav: function(e) {
       if (this.data.source_open) {
         this.setData({
           source_open: false,
@@ -82,7 +85,7 @@ Component({
         })
       }
     },
-    tapStyleNav: function (e) {
+    tapStyleNav: function(e) {
       if (this.data.style_open) {
         this.setData({
           source_open: false,
@@ -102,7 +105,7 @@ Component({
       }
       console.log(e.target)
     },
-    tapFilterNav: function (e) {
+    tapFilterNav: function(e) {
       if (this.data.filter_open) {
         this.setData({
           source_open: false,
@@ -122,67 +125,94 @@ Component({
       }
     },
 
-    selectDistrictLeft: function (e) {
+
+    selectDefaltDistrictLeft(model) {
+      if (!model) {
+        return
+      }
+      var model = model.childModel;
+      var selectedId = model.id
+      var selectedTitle = model.title;
+      this.setData({
+        dropDownMenuDistrictDataRight: model ? model : '',
+        district_left_select: selectedId,
+        district_right_select: '',
+      })
+    },
+
+    selectDistrictLeft: function(e) {
       var model = e.target.dataset.model.childModel;
       var selectedId = e.target.dataset.model.id
       var selectedTitle = e.target.dataset.model.title;
       this.setData({
-        dropDownMenuDistrictDataRight: model==null?"":model,
+        dropDownMenuDistrictDataRight: model ? model : '',
         district_left_select: selectedId,
         district_right_select: '',
       })
-      if (model == null || model.length == 0) {
-        this.closeHyFilter();
-        this.triggerEvent("selectedItem", { index: this.data.shownavindex, selectedId: selectedId, selectedTitle: selectedTitle })
-      }
     },
 
-    selectDistrictRight: function (e) {
+    selectDistrictRight: function(e) {
       var selectedId = e.target.dataset.model.id
       var selectedTitle = e.target.dataset.model.title;
       this.closeHyFilter();
       this.setData({
         district_right_select: selectedId,
-        district_right_select_name:selectedTitle
+        district_right_select_name: selectedTitle
       })
-      this.triggerEvent("selectedItem", { index: this.data.shownavindex, selectedId: selectedId, selectedTitle: selectedTitle })
+      this.triggerEvent("selectedItem", {
+        index: this.data.shownavindex,
+        selectedId: selectedId,
+        selectedTitle: selectedTitle
+      })
     },
 
-    selectSourceItem: function (e) {
+    selectSourceItem: function(e) {
       var selectedId = e.target.dataset.model.id
       var selectedTitle = e.target.dataset.model.title;
       this.closeHyFilter();
       this.setData({
         selected_source_id: selectedId,
-        selected_source_name:selectedTitle
+        selected_source_name: selectedTitle
       })
-      this.triggerEvent("selectedItem", { index: this.data.shownavindex, selectedId: selectedId, selectedTitle: selectedTitle })
+      this.triggerEvent("selectedItem", {
+        index: this.data.shownavindex,
+        selectedId: selectedId,
+        selectedTitle: selectedTitle
+      })
     },
 
-    selectFilterItem: function (e) {
+    selectFilterItem: function(e) {
       var selectedId = e.target.dataset.model.id
       var selectedTitle = e.target.dataset.model.title;
       this.closeHyFilter();
       this.setData({
         selected_filter_id: selectedId,
-        selected_filter_name:selectedTitle
+        selected_filter_name: selectedTitle
       })
-      this.triggerEvent("selectedItem", { index: this.data.shownavindex, selectedId: selectedId, selectedTitle: selectedTitle })
+      this.triggerEvent("selectedItem", {
+        index: this.data.shownavindex,
+        selectedId: selectedId,
+        selectedTitle: selectedTitle
+      })
     },
 
-    selectStyleItem: function (e) {
+    selectStyleItem: function(e) {
       var selectedId = e.target.dataset.model.id
       var selectedTitle = e.target.dataset.model.title;
       this.closeHyFilter();
       this.setData({
         selected_style_id: selectedId,
-        selected_style_name:selectedTitle
+        selected_style_name: selectedTitle
       })
-      this.triggerEvent("selectedItem", { index: this.data.shownavindex, selectedId: selectedId, selectedTitle: selectedTitle })
+      this.triggerEvent("selectedItem", {
+        index: this.data.shownavindex,
+        selectedId: selectedId,
+        selectedTitle: selectedTitle
+      })
     },
-    
+
     /**关闭筛选 */
-    closeHyFilter: function () {
+    closeHyFilter: function() {
       if (this.data.district_open) {
         this.setData({
           district_open: false,
@@ -197,16 +227,14 @@ Component({
           district_open: false,
           filter_open: false,
         })
-      }
-      else if (this.data.style_open) {
+      } else if (this.data.style_open) {
         this.setData({
           source_open: false,
           style_open: false,
           district_open: false,
           filter_open: false,
         })
-      }
-      else if (this.data.filter_open) {
+      } else if (this.data.filter_open) {
         this.setData({
           source_open: false,
           style_open: false,
@@ -217,9 +245,9 @@ Component({
     },
   },
   //组件生命周期函数，在组件实例进入页面节点树时执行
-  attached: function () {
-    
-    
+  attached: function() {
+
+
   },
 
 })
